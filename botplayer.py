937daa78e2b16263907bb1a228812274d393d352
felixschwebel <schwebel.felix@gmail.com>
2022-12-01 09:12:55 +0100

    start the game with sending the space bar key

--- botplayer.py
+++ botplayer.py
@@ -1,12 +1,33 @@
+import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import os
 
 chrome_webdriver = os.environ["CHROME_DRIVER"]
-URL = "chrome://dino/"
+URL = "chrome://dino"
 
 
 class DinosaurGameBot:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=chrome_webdriver)
-        self.driver.get(URL)
+
+        # Catch the WebDriverException that's returned because of the Chrome URL
+        try:
+            self.driver.get(URL)
+        except selenium.common.exceptions.WebDriverException:
+            pass
+        # Start the game with the space bar after 2 seconds
+        time.sleep(2)
+        self.body = self.driver.find_element(By.TAG_NAME, 'body')
+        self.body.send_keys(Keys.SPACE)
+
+    def jump(self):
+        self.body.send_keys(Keys.ARROW_UP)
+
+    def duck(self):
+        self.body.send_keys(Keys.ARROW_DOWN)
+
+    def check_for_obstacles(self):
+        pass
